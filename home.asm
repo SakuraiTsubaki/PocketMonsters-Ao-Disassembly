0d5b00e49bb10eeaed5f271ce4e8c28b10629f5d
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 14:00:29 +0900

    Add ROM0 assembly entry file

---
+++ home.asm
@@ -0,0 +1,9 @@
+; Pocket Monsters Ao / Pokemon Blue ROM0 reconstruction.
+; Build with exactly one version define: _JP, _EN, _DE, _FR, _IT, or _ES.
+
+INCLUDE "constants/hardware.asm"
+
+INCLUDE "home/header.asm"
+INCLUDE "home/high_home.asm"
+INCLUDE "home/start.asm"
+INCLUDE "data/maps/map_header_pointers.asm"
